1e050f9d370f3b3d2fae0dd8299c080de719a47b
rick-ctril <perluizhenrique@gmail.com>
2022-02-24 17:41:31 -0300

    Calculandor de descontos

---
+++ calculando-descontos.py
@@ -0,0 +1,10 @@
+#pedir valor do produto
+value_original = float(input('Valor original do produto: R$ '))
+#desconto ganho de 20%
+discount_value = value_original * 0.2
+#Valor com desconto incluso
+new_value = value_original * 0.80
+#Exibindo tudo
+print('Valor original: R$', value_original)
+print('Desconto ganho: R$', discount_value)
+print('Valor com desconto', new_value)
